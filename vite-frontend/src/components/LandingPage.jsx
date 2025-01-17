0ff9ad7d612f04aa27b7639264581fd23907eeda
Thom Lamb <fitnessevangelist@gmail.com>
2025-01-17 16:33:36 -0500

    landing page works abiout to add genkit in

---
+++ vite-frontend/src/components/LandingPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+// import './LandingPage.css'; // Create this file for styling
+
+const LandingPage = () => {
+    console.log('LandingPage.jsx');
+  return (
+    <div>
+      <h1>Wilo.ai</h1>
+      <p>Trainers create workouts.</p>
+      <p>Clients complete them.</p>
+      <p>Progress tracked.</p>
+      <p>AI learns and suggests.</p>
+      <p>Simple as that.</p>
+      
+    </div>
+  );
+};
+
+export default LandingPage;
