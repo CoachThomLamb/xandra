3f9af839457771a54acb78336f5c39cbc554e49b
Thom Lamb <fitnessevangelist@gmail.com>
2025-01-19 06:52:01 -0500

    Removing async code from UserFood for now bc it might cause an error

--- vite-frontend/src/components/UserNutrition.jsx
+++ vite-frontend/src/components/UserNutrition.jsx
@@ -5,34 +5,34 @@ import { db } from '../firebaseConfig';
 
 const UserNutrition = () => {
   const { userId } = useParams();
-  const [nutrition, setNutrition] = useState(null);
-  const [error, setError] = useState(null);
+//   const [nutrition, setNutrition] = useState(null);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchNutrition = async () => {
-      try {
-        const nutritionDoc = await getDoc(doc(db, 'users', userId, 'nutrition'));
-        if (nutritionDoc.exists()) {
-          setNutrition(nutritionDoc.data());
-        } else {
-          setError('Nutrition data not found');
-        }
-      } catch (error) {
-        console.error('Error fetching nutrition data:', error);
-        setError('Error fetching nutrition data. Please try again later.');
-      }
-    };
+//   useEffect(() => {
+//     const fetchNutrition = async () => {
+//       try {
+//         const nutritionDoc = await getDoc(doc(db, 'users', userId, 'nutrition'));
+//         if (nutritionDoc.exists()) {
+//           setNutrition(nutritionDoc.data());
+//         } else {
+//           setError('Nutrition data not found');
+//         }
+//       } catch (error) {
+//         console.error('Error fetching nutrition data:', error);
+//         setError('Error fetching nutrition data. Please try again later.');
+//       }
+//     };
 
-    fetchNutrition();
-  }, [userId]);
+//     fetchNutrition();
+//   }, [userId]);
 
-  if (error) {
-    return <div>{error}</div>;
-  }
+//   if (error) {
+//     return <div>{error}</div>;
+//   }
 
-  if (!nutrition) {
-    return <div>Loading...</div>;
-  }
+//   if (!nutrition) {
+//     return <div>Loading...</div>;
+//   }
 
   return (
     <div>
