e50fe636cedcb5ffacea1a9db889d7f420023d4f
Thom Lamb <fitnessevangelist@gmail.com>
2025-02-14 15:22:59 -0500

    initial expo app- prob broke

---
+++ wilo-ai/types/types.ts
@@ -0,0 +1,37 @@
+export interface ExerciseDefinition {
+    id: string;
+    name: string;
+    videoURL?: string;
+  }
+  
+  export interface Set {
+    id?: string;
+    setNumber: number;
+    reps: number;
+    load: number;
+    completed?: boolean;
+  }
+  
+  export interface ExerciseInstance {
+    id?: string;
+    exerciseId: string;
+    name: string;
+    orderBy: number;
+    sets: Set[];
+    notes?: string;
+    clientVideoURL?: string;
+    coachNotes?: string; // Add this line
+  }
+  
+  export interface Workout {
+    id: string
+    coachNotes: string;
+    exercises: ExerciseInstance[];
+    notes?: Record<number, string>;
+    videoURL?: string;
+    title?: string;
+    completed?: boolean;
+    completedAt?: Date;
+    dueDate?: Date; // Add this line
+  }
+  
