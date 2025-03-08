5ad939116e8680a951ded3c9d151e2dece8c6bc4
Thom Lamb <fitnessevangelist@gmail.com>
2025-03-07 19:26:53 -0500

    Adds tests

--- vite-frontend/vite.config.js
+++ vite-frontend/vite.config.js
@@ -1,3 +1,7 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
@@ -28,5 +32,12 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true, 
+  },
+  
 });
